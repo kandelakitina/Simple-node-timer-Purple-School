a6d07504602273dc766696c2607d6853752cd282
boticelli <kandelakitina@gmail.com>
2025-12-29 14:30:51 +0300

    First implementation

---
+++ index.js
@@ -0,0 +1,38 @@
+async function timeout(ms) {
+	console.log(`Waiting for ${ms} milliseconds...`);
+	return new Promise((resolve) =>
+		setTimeout(() => {
+			console.log(`Waited for ${ms} milliseconds.`);
+			resolve();
+		}, ms),
+	);
+}
+
+function parseTimeString(timeStr) {
+	const timeParts = timeStr.split(" ");
+	let totalMs = 0;
+
+	for (const part of timeParts) {
+		const unit = part.slice(-1);
+		const value = parseInt(part.slice(0, -1));
+
+		switch (unit) {
+			case "h":
+				totalMs += value * 60 * 60 * 1000;
+				break;
+			case "m":
+				totalMs += value * 60 * 1000;
+				break;
+			case "s":
+				totalMs += value * 1000;
+				break;
+		}
+	}
+
+	return totalMs;
+}
+
+const args = process.argv.slice(2);
+const timeStr = args.join(" ");
+const ms = parseTimeString(timeStr);
+timeout(ms);
